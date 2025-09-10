75f96af2f69eca3a34d9fcd21c9a65a4cb06b9e9
yksab <baskykeefe@gmail.com>
2025-09-10 17:50:22 +1200

    ab

---
+++ scripts/getdates.js
@@ -0,0 +1,17 @@
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+
+// Match 'timestamp' in 'last_modif=timestamp'
+// e.g. '1687964614822' in 'last_modif=1687964614822'
+const pattern = /last_modif\s*=\s*([^;]*)/;
+
+if (
+  Date.parse(document.lastModified) >
+  (parseFloat(document.cookie.match(pattern)?.[1]) || 0)
+) {
+  document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${
+    location.pathname
+  }`;
+  alert("This page has changed!");
+}
